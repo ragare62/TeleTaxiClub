58f81b4226d9a49aa8e78cdb612932f73c412ae8
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2014-06-17 12:24:19 +0200

    Ordenando las librerias (1)
    se crean las api en ficheros separados

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var cors = require('cors');
-var formidable = require('formidable');
+
 // leyendo los parámetros
 var config = require('./config.json')
-// apyo a la api
-var miembros = require('./lib/miembros');
-var premios = require('./lib/premios');
-// manejo de ficheros
-var fs = require('fs');
+
+// apoyo a las api
+var miembros_api = require('./lib/miembros_api');
+var premios_api = require('./lib/premios_api');
+var uploads_api = require('./lib/uploads_api');
 
 // utilizar el parseador
 app.use(bodyParser());
@@ -45,70 +45,13 @@ router.get('/', function(req, res) {
 // INICIO Rutas relacionadas con miembros
 //===================================================================
 router.route('/miembros')
-    .get(function(req, res) {
-        miembros.get(null, function(err, miembros) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                res.json(miembros);
-            }
-        });
-    })
-    .post(function(req, res) {
-        var miembro = req.body;
-        miembros.post(miembro, function(err, miembro) {
-            if (err) {
-                res.send(400, err.message);
-            } else {
-                res.json(miembro);
-            }
-        });
-    });
+    .get(miembros_api.getMiembros)
+    .post(miembros_api.postMiembro);
 
 router.route('/miembros/:miembro_id')
-    .get(function(req, res) {
-        miembros.get(req.params.miembro_id, function(err, miembros) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                if (miembros.length === 0) {
-                    res.send(404, 'Miembro no encontrado');
-                } else {
-                    res.json(miembros[0]);
-                }
-            }
-        });
-    })
-    .put(function(req, res) {
-        var miembro = req.body;
-        var id = req.params.miembro_id;
-        if (id != miembro.idMiembro) {
-            res.send(400, 'El ID pasado y el del objeto no coinciden');
-            return;
-        }
-        miembros.put(miembro, function(err, miembro) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                res.json(miembro);
-            }
-        });
-    })
-    .delete(function(req, res) {
-        var miembro = req.body;
-        var id = req.params.miembro_id;
-        if (id != miembro.idMiembro) {
-            res.send(400, 'El ID pasado y el del objeto no coinciden');
-            return;
-        }
-        miembros.delete(miembro, function(err, miembro) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                res.json(miembro);
-            }
-        });
-    });
+    .get(miembros_api.getMiembro)
+    .put(miembros_api.putMiembro)
+    .delete(miembros_api.deleteMiembro);
 
 //===================================================================
 // FIN Rutas relacionadas con miembros
@@ -117,70 +60,13 @@ router.route('/miembros/:miembro_id')
 // INICIO Rutas relacionadas con premios
 //===================================================================
 router.route('/premios')
-    .get(function(req, res) {
-        premios.get(null, function(err, premios) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                res.json(premios);
-            }
-        });
-    })
-    .post(function(req, res) {
-        var premio = req.body;
-        premios.post(premio, function(err, premio) {
-            if (err) {
-                res.send(400, err.message);
-            } else {
-                res.json(premio);
-            }
-        });
-    });
+    .get(premios_api.getPremios)
+    .post(premios_api.postPremio);
 
 router.route('/premios/:premio_id')
-    .get(function(req, res) {
-        premios.get(req.params.premio_id, function(err, premios) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                if (premios.length === 0) {
-                    res.send(404, 'Premio no encontrado');
-                } else {
-                    res.json(premios[0]);
-                }
-            }
-        });
-    })
-    .put(function(req, res) {
-        var premio = req.body;
-        var id = req.params.premio_id;
-        if (id != premio.idPremio) {
-            res.send(400, 'El ID pasado y el del objeto no coinciden');
-            return;
-        }
-        premios.put(premio, function(err, premio) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                res.json(premio);
-            }
-        });
-    })
-    .delete(function(req, res) {
-        var premio = req.body;
-        var id = req.params.premio_id;
-        if (id != premio.idPremio) {
-            res.send(400, 'El ID pasado y el del objeto no coinciden');
-            return;
-        }
-        premios.delete(premio, function(err, premio) {
-            if (err) {
-                res.send(500, err.message);
-            } else {
-                res.json(premio);
-            }
-        });
-    });
+    .get(premios_api.getPremio)
+    .put(premios_api.putPremio)
+    .delete(premios_api.deletePremio);
 
 //===================================================================
 // FIN Rutas relacionadas con premios
@@ -189,46 +75,8 @@ router.route('/premios/:premio_id')
 //====================================================================
 
 router.route('/uploads/:premio_id')
-    .post(function(req, res) {
-        var idPremio = req.params.premio_id;
-        //console.log("IDPREMIO:", idPremio)
-        if (!isFormData(req)) {
-            res.statusCode = 404;
-            res.end('Petición incorrecta: se esperaba multipart/form-data');
-            return;
-        }
+    .post(uploads_api.uploadImagenPremio(__dirname));
 
-        var form = new formidable.IncomingForm();
-        //form.on('progress', function(bytesReceived, bytesExpected) {
-        //    var percent = Math.floor(bytesReceived / bytesExpected * 100);
-        //    console.log(percent);
-        //});
-
-        form.parse(req, function(err, fields, files) {
-            //console.log(fields);
-            //console.log(files);
-            fs.readFile(files.file.path, function(err, data) {
-                var newPath = __dirname + "\\public\\images\\" + files.file.name;
-                //console.log("FICHERO: ", newPath)
-                //fs.writeFile(newPath, data, function(err) {
-                //    if (err) throw err;
-                //});
-                fs.rename(files.file.path, newPath, function(err) {
-                    if (err) throw err;
-                });
-            });
-            var data = {};
-            data.Status = 1
-            data.FullUrl = req.protocol + '://' + req.get('host') + '//images//' + files.file.name;
-            data.FileName = files.file.name;
-            res.json(data);
-        });
-    });
-
-function isFormData(req) {
-    var type = req.headers['content-type'] || '';
-    return 0 == type.indexOf('multipart/form-data');
-}
 
 //====================================================================
 // FIN Rutas relacionadas con carga de ficheros
